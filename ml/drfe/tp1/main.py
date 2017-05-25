3b497a36a8da1df70f1f4c43c8783e6ff16ba0a6
Sarasvati Moutoucomarapoulé <moutou_s@epita.fr>
2017-05-25 17:05:56 +0200

    drfe: tp1: refactor
    * main.py: Here.

--- ml/drfe/tp1/main.py
+++ ml/drfe/tp1/main.py
@@ -15,16 +15,31 @@ test_data, test_labels = load_mnist(dataset='testing', path='./')
 train_data = np.reshape(train_data, (60000, 28 * 28)).T
 test_data  = np.reshape(test_data,  (10000, 28 * 28)).T
 
+colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab',
+          'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
+
+def scatter_data(nbclasses, data, labels):
+   for i in range(nbclasses):
+        d = data[:, np.where(labels == i)[0]]
+        plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
+   plt.legend()
+   plt.show()
+
+def perf(classifier, train, test):
+    classifier.train(train, train_labels)
+    out = classifier.process(test)
+    print("Rate:",
+          ((out == test_labels).sum() / test_labels.shape[0]))
+
 def question1(classifier):
     # Display mean images.
     vz.plotGroupImages(classifier.mean[:,:])
     # Show images.
     plt.show()
 
-def qquestion3(classifier):
-    out = classifier.process(test_data)
-    perf = (out == test_labels).sum() / test_labels.shape[0]
-    print("Lin1 perf:", perf)
+def question3(classifier):
+    print("* Lin1 performance")
+    perf(classifier, train_data, test_data)
 
 def question4_1():
     pca = Pca()
@@ -35,25 +50,21 @@ def question4_2(pca):
     plt.plot(pca.eigenval)
     plt.show()
 
-def question4_3(pca):
-    rates = [0.75, 0.90, 0.95, 0.995]
+def question4_3(pca, rates):
     nb_eigenval = pca.computecompression(rates)
     print(nb_eigenval)
     return nb_eigenval
 
-def question4_4(nb_eigenval, rates):
+def question4_4(classifier, pca, nb_eigenval, rates):
     for i in range(len(rates)):
         if nb_eigenval[i] is None :
             break
 
-        proj_train_data = pca.project(train_data, nb_eigenval[i])
-        proj_test_data = pca.project(test_data, nb_eigenval[i])
-        Q1.train(proj_train_data, train_labels)
+        pca_proj_train = pca.project(train_data, nb_eigenval[i])
+        pca_proj_test = pca.project(test_data, nb_eigenval[i])
 
-        out = Q1.process(proj_test_data)
-        perf = (out == test_labels).sum() / test_labels.shape[0]
-
-        print("Lin1 proj with ", rates[i], " of info, perf: ", perf)
+        print("* Lin1, projection with", rates[i], "of info, PCA performance")
+        perf(classifier, pca_proj_train, pca_proj_test)
 
 def question4_5(pca):
     vz.plotGroupImages(pca.eigenvect[:, :10])
@@ -61,187 +72,95 @@ def question4_5(pca):
 
 def question4_6(classifier, pca):
     proj_train_data = pca.project(train_data, 2)
+    scatter_data(classifier.nbclasses, proj_train_data, train_labels)
+
+def question4_7(classifier, pca):
     proj_test_data = pca.project(test_data, 2)
-    classifier.train(proj_train_data, train_labels)
-
-    colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab', 'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
-    for i in range(classifier.nbclasses):
-        d = proj_train_data[:, np.where(train_labels == i)[0]]
-        plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
-
-    plt.legend()
-    plt.show()
-
-    return proj_test_data
-
-def question4_7(classifier, proj_test_data):
-    out = classifier.process(proj_test_data)
-    for i in range(classifier.nbclasses):
-        d = proj_test_data[:, np.where(out == i)[0]]
-        plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
-
-    plt.legend()
-    plt.show()
+    scatter_data(classifier.nbclasses, proj_test_data, test_labels)
 
 def part2_question1():
     lda = Lda()
     lda.train(train_data, train_labels)
     return lda
 
+def part2_question2(classifier, lda):
+    # Computes 1...9 dimensions
+    for i in range(1, 10):
+        lda_proj_train = lda.project(train_data, i)
+        lda_proj_test = lda.project(test_data, i)
+
+        print("* Lin1, projection with", i, "dimensions, LDA performance")
+        perf(classifier, lda_proj_train, lda_proj_test)
+
 def part2_question3(lda):
     vz.plotGroupImages(lda.eigenvect[:, :10].real)
     plt.show()
 
 def part2_question4(classifier, lda):
-    proj_train_data = lda.project(train_data, 2)
-    proj_test_data = lda.project(test_data, 2)
-    classifier.train(proj_train_data, train_labels)
-    colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab', 'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
-    for i in range(classifier.nbclasses):
-        d = proj_train_data[:, np.where(train_labels == i)[0]]
-        plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
+    lda_proj_train = lda.project(train_data, 2)
+    scatter_data(classifier.nbclasses, lda_proj_train, train_labels)
 
-    plt.legend()
-    plt.show()
+def part2_question5(classifier, lda):
+    lda_proj_test = lda.project(test_data, 2)
+    scatter_data(classifier.nbclasses, lda_proj_test, test_labels)
+
+def part2_question7(classifier, pca, lda, pca_nb_dim):
+    # Project with PCA.
+    pca_proj_train = pca.project(train_data, pca_nb_dim)
+    pca_proj_test = pca.project(test_data, pca_nb_dim)
+
+    # Train LDA.
+    lda.train(pca_proj_train, train_labels)
+
+    # Computes 1...9 dimensions
+    for i in range(1, 10):
+        lda_proj_train = lda.project(pca_proj_train, i)
+        lda_proj_test = lda.project(pca_proj_test, i)
+
+        print("* Lin1, PCA with", pca_nb_dim, "dimensions, LDA with", i,
+              "dimensions, performance")
+        perf(classifier, lda_proj_train, lda_proj_test)
+
+    scatter_data(classifier.nbclasses, lda_proj_test, test_labels)
 
 if __name__ == '__main__':
     # Gaussian classifier, euclidian distance.
     Q1 = Lin1_Gauss()
     Q1.train(train_data, train_labels)
 
-    # Question 1.
-    # Display mean images.
-    # vz.plotGroupImages(Q1.mean[:,:])
-    # Show images.
-    # plt.show()
+
+    ######### PART 1 ###########
 
     # question1(Q1)
 
-    # Question 3
-    # out = Q1.process(test_data)
-    # perf = (out == test_labels).sum() / test_labels.shape[0]
-    # print("Lin1 perf:", perf)
-
     # question3(Q1)
 
-    # Question 4
-    # Question A.
-    #pca = Pca()
-    #pca.train(train_data)
     pca = question4_1()
 
-    # Question B.
-    # plt.plot(pca.eigenval)
-    # plt.show()
     # question4_2(pca)
 
-    # Question C.
-    # rates = [0.75, 0.90, 0.95, 0.995]
-    # nb_eigenval = pca.computecompression(rates)
-    # print(nb_eigenval)
-    # nb_eigenval = question4_3(pca)
+    rates = [0.75, 0.90, 0.95, 0.995]
 
-    # Question D.
-    # for i in range(len(rates)):
-        # if nb_eigenval[i] is None :
-            # break
+    # nb_eigenval = question4_3(pca, rates)
 
-        # proj_train_data = pca.project(train_data, nb_eigenval[i])
-        # proj_test_data = pca.project(test_data, nb_eigenval[i])
-        # Q1.train(proj_train_data, train_labels)
+    # question4_4(Q1, pca, nb_eigenval, rates)
 
-        # out = Q1.process(proj_test_data)
-        # perf = (out == test_labels).sum() / test_labels.shape[0]
-
-        # print("Lin1 proj with ", rates[i], " of info, perf: ", perf)
-    # question4_4(nb_eigenval, rates)
-
-    # Question E.
-    # vz.plotGroupImages(pca.eigenvect[:, :10])
-    # plt.show()
     # question4_5(pca)
 
-    # Question F.
-    # proj_train_data = pca.project(train_data, 2)
-    # proj_test_data = pca.project(test_data, 2)
-    # Q1.train(proj_train_data, train_labels)
+    # question4_6(Q1, pca)
 
-    # colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab', 'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
-    # for i in range(Q1.nbclasses):
-        # d = proj_train_data[:, np.where(train_labels == i)[0]]
-        # plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
-
-    # plt.legend()
-    # plt.show()
-    # proj_test_data = question4_6(Q1, pca)
-
-    # Question G.
-    # out = Q1.process(proj_test_data)
-    # for i in range(Q1.nbclasses):
-        # d = proj_test_data[:, np.where(out == i)[0]]
-        # plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
-
-    # plt.legend()
-    # plt.show()
-    # question4_7(Q1, proj_test_data)
+    # question4_7(Q1, pca)
 
     ########### PART 2 ###########
 
-    # Question 2.1 et 2.2
-    # lda = Lda()
-    # lda.train(train_data, train_labels)
     lda = part2_question1()
 
-    # Computes 1...9 dimensions
-    # for i in range(1, 10):
-    #     lda_projected_train = lda.project(train_data, i)
-    #     lda_projected_data = lda.project(test_data, i)
-    #     Q1.train(lda_projected_train, train_labels)
-    #     out = Q1.process(lda_projected_data)
-    #     print(i, " Rate = ", ((out == test_labels).sum() / test_labels.shape[0]))
+    # part2_question2(Q1, lda)
 
-    # Question 2.3
-    # vz.plotGroupImages(lda.eigenvect[:, :10].real)
-    # plt.show()
-    # part2_question1(lda)
+    # part2_question3(lda)
 
-    # Question 2.4
-    # proj_train_data = lda.project(train_data, 2)
-    # proj_test_data = lda.project(test_data, 2)
-    # Q1.train(proj_train_data, train_labels)
-    # colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab', 'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
-    # for i in range(Q1.nbclasses):
-    #     d = proj_train_data[:, np.where(train_labels == i)[0]]
-    #     plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
+    # part2_question4(Q1, lda)
 
-    # plt.legend()
-    # plt.show()
-    # part2_question4(classifier, lda)
+    # part2_question5(Q1, lda)
 
-    # Question 2.5
-
-    # Question 2.7
-    pc_nb_dim = 87
-    pca_proj_train_data = pca.project(train_data, pc_nb_dim)
-    pca_proj_test_data = pca.project(test_data, pc_nb_dim)
-    # Processes train data obtained from PCA
-    lda.train(pca_proj_train_data, train_labels)
-    lda_projected_train = lda.project(pca_proj_train_data, 2)
-    lda_projected_data = lda.project(pca_proj_test_data, 2)
-    Q1.train(lda_projected_train, train_labels)
-
-    # Computes 1...9 dimensions
-    for i in range(1, 10):
-        lda_projected_train = lda.project(pca_proj_train_data, i)
-        lda_projected_data = lda.project(pca_proj_test_data, i)
-        Q1.train(lda_projected_train, train_labels)
-        out = Q1.process(lda_projected_data)
-        print(i, " Rate = ", ((out == test_labels).sum() / test_labels.shape[0]))
-
-    # colors = ['gold', 'green', 'black', 'magenta', 'teal', 'olivedrab', 'forestgreen', 'darkmagenta', 'khaki', 'darkgray']
-    # for i in range(Q1.nbclasses):
-    #     d = lda_projected_data[:, np.where(train_labels == i)[0]]
-    #     plt.scatter(d[0,:], d[1,:], c=colors[i], s=0.6, marker='.', label=i)
-
-    # plt.legend()
-    # plt.show()
+    part2_question7(Q1, pca, lda, 87)
