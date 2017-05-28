00a86ddbf6270d9e12ef4f63706048a6964c8d98
David Peicho <david.peicho@epita.fr>
2017-05-28 23:50:10 +0200

    syma: Updates death to be more randomized

--- syma/src/syma/utils/Const.java
+++ syma/src/syma/utils/Const.java
@@ -1,16 +1,8 @@
 package syma.utils;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import syma.environment.Building;
-import syma.environment.Road;
-import syma.main.GridElement;
-
 public class Const {
 
 	/* HUMAN CONST */
-	public static final int MAX_AGE = 100; 
+	public static final int MAX_AGE = 90;
 	
 }
