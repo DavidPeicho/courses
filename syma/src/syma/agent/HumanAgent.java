f0135cf34a7d2d60c02c796aa2b9aec6874ddd4f
Benzaiten <moutou_s@epita.fr>
2017-05-29 12:54:33 +0100

    syma: add child watcher to parent's order
    * src/syma/agent/HumanAgent.java: Add watcher family getters.
    * src/syma/main/ContextManager.java: Spawn a child.
    * src/syma/utils/Const.java: Define parent and child weights.

--- syma/src/syma/agent/HumanAgent.java
+++ syma/src/syma/agent/HumanAgent.java
@@ -2,12 +2,19 @@ package syma.agent;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import repast.simphony.context.Context;
 import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.engine.watcher.Watch;
+import repast.simphony.engine.watcher.WatcherTriggerSchedule;
+import repast.simphony.space.graph.Network;
+import repast.simphony.space.graph.RepastEdge;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.util.ContextUtils;
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -25,15 +32,17 @@ import syma.utils.Const;
 public class HumanAgent extends AAgent {
 
 	private static Logger LOGGER = Logger.getLogger(HumanAgent.class.getName());
-	
+
 	private int age_;
 	private boolean gender_;
 	
 	private Building home_;
 	private WorkPlace workplace_;
 	
+	private boolean order_;
+
 	private float maxAge_;
-	
+
 	private UpdateListener yearListener_ = new UpdateListener() {
 
 		@Override
@@ -42,12 +51,13 @@ public class HumanAgent extends AAgent {
 		}
 		
 	};
-	
+
 	public HumanAgent(int x, int y, Grid<GridElement> grid, int age, boolean gender) {
 		super(x, y, grid);
 		age_ = age;
 		gender_ = gender;
 		maxAge_ = Const.MAX_AGE + (int)((Math.random() * 30) - 15);
+		order_ = false;
 	}
 	
 	@Override
@@ -55,6 +65,12 @@ public class HumanAgent extends AAgent {
 	public void step() {
 		super.step();
 		double deathRate = ((float)age_ / (float)maxAge_) * Math.random();
+
+		// Test Watcher
+		if (!order_ && id_ == 1 && age_ >= 30) {
+			order_ = true;
+		}
+
 		if (deathRate >= 0.85) {
 			die();
 		}
@@ -67,6 +83,21 @@ public class HumanAgent extends AAgent {
 		context.remove(this);
 	}
 	
+	@Watch(watcheeClassName="syma.agent.HumanAgent",
+		   watcheeFieldNames="order_",
+		   query="linked_from 'genealogy'",
+		   whenToTrigger=WatcherTriggerSchedule.IMMEDIATE)
+	public void react() {
+		Stream<AAgent> ps = getParents().filter((AAgent a) -> ((HumanAgent)a).getOrder());
+		Optional<AAgent> p = ps.findFirst();
+
+		if (p.isPresent()) {
+			HumanAgent hp = (HumanAgent)p.get();
+			System.out.println("I am " + id_);
+			System.out.println("I received an order from " + hp.getID());
+		}
+	}
+
 	/* GETTERS // SETTERS */
 	
 	public int getAge() {
@@ -89,6 +120,15 @@ public class HumanAgent extends AAgent {
 		return workplace_;
 	}
 	
+	public void setOrder(boolean order) {
+		order_ = order;
+	}
+
+	public boolean getOrder() {
+		return order_;
+	}
+
+
 	public UpdateListener getYearListener() {
 		return yearListener_;
 	}
@@ -143,5 +183,25 @@ public class HumanAgent extends AAgent {
 		}
 		while (!m.isEmpty())
 			behaviors_.add(m.pop());
+	}
+
+	public Stream<AAgent> getCompanions() {
+		return getRelatedAgent(Const.MARRIEDTO, true);
+	}
+
+	public Stream<AAgent> getParents() {
+		return getRelatedAgent(Const.PARENTOF, true);
+	}
+
+	public Stream<AAgent> getChildren() {
+		return getRelatedAgent(Const.PARENTOF, false);
+	}
+
+	public Stream<AAgent> getRelatedAgent(double relationship, boolean in) {
+		Network n = (Network)ContextUtils.getContext(this).getProjection("genealogy");
+		Iterable<RepastEdge> edges = in ? n.getInEdges(this) : n.getOutEdges(this);
+		return StreamSupport.stream(edges.spliterator(), false)
+				.filter((RepastEdge e) -> e.getWeight() == relationship)
+				.map((RepastEdge e) -> (AAgent)(in ? e.getSource() : e.getTarget()));
 	}
 }
--- syma/src/syma/utils/Const.java
+++ syma/src/syma/utils/Const.java
@@ -5,4 +5,7 @@ public class Const {
 	/* HUMAN CONST */
 	public static final int MAX_AGE = 90;
 	
+	public static final double MARRIEDTO = 1;
+
+	public static final double PARENTOF = 2;
 }
