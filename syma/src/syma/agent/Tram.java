fd8d17cf1bd59f45c003fc9b02b90f90dfe8cbe1
Aurélien Leandri <Aurlien Leandri>
2017-05-31 14:21:38 +0200

    Add cycle for tram and computing for bus stops

--- syma/src/syma/agent/Tram.java
+++ syma/src/syma/agent/Tram.java
@@ -1,23 +1,62 @@
 package syma.agent;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import syma.environment.BusStop;
+import syma.environment.Road;
+import syma.goal.MoveTo;
 import syma.main.GridElement;
 
 public class Tram extends AAgent {
 	ArrayList<AAgent> passengers;
+	ArrayList<BusStop> stops;
+	ArrayList<GridElement> roadStops;
 
-	public Tram(Grid<GridElement> grid) {
+	public Tram(Grid<GridElement> grid, ArrayList<BusStop> busStops) {
 		super(grid);
-		// TODO Auto-generated constructor stub
+		stops = busStops;
+		roadStops = new ArrayList<>();
+		computeRoadStops();
 	}
 	
-	private void takeIn(int posx, int posy) {
-		
-		for (AAgent agent : passengers) {
-			
+	private void computeRoadStops() {
+		for (BusStop e : stops) {
+			int x = e.getX();
+			int y = e.getY();
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					if (i == j || i == 0 && j == 0) 
+						continue;
+					int w = x + i;
+					int h = y + j;
+					Iterable<GridElement> neighbor = grid_.getObjectsAt(w, h);
+					Iterator<GridElement> it = neighbor.iterator();
+					if (!neighbor.iterator().hasNext())
+						continue;
+					while (it.hasNext()) {
+						Object o = it.next();
+						if (o instanceof Road) {
+							roadStops.add(((Road)o));
+							break;
+						}
+					}
+				}
+			}
 		}
 	}
-
+	
+	public void computeCycle() {
+		if (roadStops.isEmpty())
+			return;
+		/*
+		MoveTo goal = new MoveTo(this, roadStops.get(0), grid_);
+		for (int i = 1; i < roadStops.size(); i++) {
+			MoveTo nextGoal = new MoveTo(this, roadStops.get(i), grid_);
+		}
+		*/
+	}
+	
 }
---
+++ syma/src/syma/environment/BusStop.java
@@ -0,0 +1,12 @@
+package syma.environment;
+
+import repast.simphony.space.grid.Grid;
+import syma.main.GridElement;
+
+public class BusStop extends AFixedGeography {
+
+	public BusStop(Grid<GridElement> grid) {
+		super(grid);
+	}
+
+}
