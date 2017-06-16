4ba9f7b536cd3f592cb5146321141dae2442c176
David Peicho <david.peicho@gmail.com>
2017-06-16 12:14:19 +0200

    txt-mining: Add simple search

--- text-mining/main.py
+++ text-mining/main.py
@@ -1,21 +1,36 @@
+import argparse
+
 from document import Document
 from normalizer_processor import NormalizerProcessor
 from tokenized_document import TokenizedDocument
 from posting import Posting
+from index import Index
+from searcher import Searcher
 
-if __name__ == "__main__":
-    documents = Document.fetch("./assets/emails/alt.atheism", True)
+documents = None
+processors = []
+tokenized_documents = []
 
-    processors = []
-    processors.append(NormalizerProcessor())
-    
-    # Normalizes every loaded documents
-    tokenized_documents = []
+def tokenize_all():
     for d in documents:
         tokenized_doc = TokenizedDocument.analyze(d, processors)
         tokenized_documents.append(tokenized_doc)
 
+# def parse_args():
+
+if __name__ == "__main__":
+    documents = Document.fetch("./assets/emails/alt.atheism", True)
+
+    processors.append(NormalizerProcessor())
+
+    # Normalizes every loaded documents
+    tokenize_all()
+
     # Creates the index by mapping every word
     # to all the documents that reference it
     posting_list = Posting.index(tokenized_documents)
-    print(posting_list["anything"].urls)
+
+    index = Index.build(posting_list)
+
+    searcher = Searcher()
+    print(searcher.search("anything", index))
