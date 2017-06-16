bd19573e8ec405f3283050100aefcb765672fbfa
David Peicho <david.peicho@gmail.com>
2017-06-16 12:47:39 +0200

    txt-mining: Add arguments parsing

--- text-mining/main.py
+++ text-mining/main.py
@@ -16,21 +16,53 @@ def tokenize_all():
         tokenized_doc = TokenizedDocument.analyze(d, processors)
         tokenized_documents.append(tokenized_doc)
 
-# def parse_args():
+def parse_args():
+    description = """
+                    A non-optimized (at all) Google.\n Obviously, I used python,
+                    so, how could it run fast? ;)
+                  """
+    arg_parser = argparse.ArgumentParser(description=description)
+    
+    # Required path to search
+    arg_parser.add_argument('-i', '--input',
+                            help='Input folder to search in',
+                            required=True)
+    arg_parser.add_argument('-s', '--search',
+                            help='Use the search to give the word to look for',
+                            required=True)
+
+    return arg_parser.parse_args()
+
+def print_result(args, urls_found):
+    
+    str = '---------------------------------------\n'
+    str += "Search Input Folder: {0}\n".format(args.input)
+    str += "Search Input Word: {0}\n".format(args.search)
+    print (str)
 
 if __name__ == "__main__":
-    documents = Document.fetch("./assets/emails/alt.atheism", True)
+
+    args = parse_args()
+
+    folder = args.input
+    wordToSearch = args.search
 
     processors.append(NormalizerProcessor())
 
+    # Fetches every documents from the input folder
+    print('[FETCHING] Reading text files from \'{0}\'...'.format(folder))
+    documents = Document.fetch(folder, True)
+
     # Normalizes every loaded documents
+    print('[PROCESSING] Normalizing words from every documents...')
     tokenize_all()
 
     # Creates the index by mapping every word
     # to all the documents that reference it
+    print('[INDEXING] Building index from words...')
     posting_list = Posting.index(tokenized_documents)
-
     index = Index.build(posting_list)
 
+    print('[SEARCHING] Searching for the word \'{0}\'...', wordToSearch)
     searcher = Searcher()
-    print(searcher.search("anything", index))
+    print_result(args, searcher.search(wordToSearch, index))
