a35976926a8a0e95c1102f34eb2ec2f30fab318b
David Peicho <david.peicho@gmail.com>
2017-07-06 00:03:32 +0200

    spark-movie: Fix sentiment analysis making wrong GridSearch

--- spark-movie/python-processing/analysis.py
+++ spark-movie/python-processing/analysis.py
@@ -1,16 +1,11 @@
+#!/usr/bin/env python3
+
 import os
 import argparse
 
-from sklearn.pipeline import Pipeline
-from sklearn.datasets import load_files
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-
 import numpy as np
+
+from sentiment_analysis.linear_svm import LinearSVM
 
 def parse_args():
     description =   """
@@ -27,71 +22,63 @@ def parse_args():
     arg_parser.add_argument('-p', '--producer',
                             help='Id of the Kafka topic to produce to. \'movie-analyzed\' by default.',
                             required=False)
+    arg_parser.add_argument('-pf', '--positive',
+                            help='Path to the training positive folder.',
+                            required=False)
+    arg_parser.add_argument('-nf', '--negative',
+                            help='Path to the training negative folder.',
+                            required=False)
+    arg_parser.add_argument('-s', '--save_db',
+                            help='Saves the learning database after training.',
+                            required=False)
+    arg_parser.add_argument('-l', '--load_db',
+                            help='Load the learning database previously trained.',
+                            required=False)
 
     return arg_parser.parse_args()
 
 if __name__ == "__main__":
 
-    NB_DATA = 12501
-    #MOVIE_REVIEWS_FOLDER = "data/reviews/train/data"
-    MOVIE_REVIEWS_FOLDER = "data/txt_sentoken"
-
-    IDX_POS = 0
-    IDX_NEG = 1
-
-    topics = {
-        'consumme': "movie-topic",
-        'produce': "movie-analyzed"
-    }
+    DATA_ROOT_FOLDER = "data/reviews/train/data/"
+    #DATA_ROOT_FOLDER = "data/txt_sentoken/"
+    POS_FOLDER = DATA_ROOT_FOLDER + "pos"
+    NEG_FOLDER = DATA_ROOT_FOLDER + "neg"
 
     args = parse_args()
-    if not(args.consummer is None):
-        topics['consumme'] = args.consummer
-    if not(args.producer is None):
-        topics['produce'] = args.producer
+    if args.consummer is None:
+        args.consummer = "movie-topic"
+    if args.producer is None:
+        args.producer = "movie-analyzed"
+    if args.positive is None:
+        args.positive = POS_FOLDER
+    if args.negative is None:
+        args.negative = NEG_FOLDER
 
-    dataset = load_files(MOVIE_REVIEWS_FOLDER, shuffle=False)
-    print("n_samples: %d" % len(dataset.data))
+    # The user did not provide a previsouly trained model,
+    # the script needs to train from any possible data folder.
+    svm = None
+    if not(args.load_db):
+        svm = LinearSVM()
+        svm.train(args.positive, args.negative)
 
-    docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_size=0.25, random_state=None)
+        if args.save_db:
+            svm.dump(args.save_db)
 
-    #test = "Lucasarts have pulled yet another beauty out of a seemingly bottomless bag of great games. If any further proof was required that they rule this genre of gaming, then this is it. Before actually playing the game, there was a little concern about how the writers were going to keep up the pace of gags after the first two games. Fears were rife that it was going to wear a bit thin.<br /><br />Play the game and see how quickly those fears are allayed. From the introductory video with Guybrush in the dodgem boat (!), to the closing stages in the funfair, the jokes just keep on coming. I was a great fan of the first two games and the other Lucasarts works (Day Of The Tentacle, Sam & Max, etc) and this one does not fail to deliver the quality. You will not be disappointed. (Well, I wasn't.)"
-    print(len(y_train))
-    #print(docs_test[0].shape)
-    #print(a.shape)
+    else:
+        svm = LinearSVM.load(args.load_db)
 
-    pipeline = Pipeline([
-        # Computes the word histogram
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        # Builds classifier
-        ('clf', LinearSVC(C=1000)),
-    ])
+    test_data = []
+    test_labels = []
+    for root, sub, files in os.walk("data/txt_sentoken/neg"):
+        for file_name in files:
+            url = os.path.join(root, file_name)
+            content = open(url, 'r', encoding='utf-8', errors='ignore').read()
+            test_data.append(content)
+            test_labels.append('neg')
 
-    parameters = {
-        'vect__ngram_range': [(1, 2)],
-    }
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-    grid_search.fit(docs_train, y_train)
-
-    # TASK: print the mean and std for each candidate along with the parameter
-    # settings for all the candidates explored by grid search.
-    #n_candidates = len(grid_search.cv_results_['params'])
-    #for i in range(n_candidates):
-        #print(i, 'params - %s; mean - %0.2f; std - %0.2f'
-                 #% (grid_search.cv_results_['params'][i],
-                    #grid_search.cv_results_['mean_test_score'][i],
-                    #grid_search.cv_results_['std_test_score'][i]))
-
-    # TASK: Predict the outcome on the testing set and store it in a variable
-    # named y_predicted
-    #y_predicted = grid_search.predict(docs_test)
-
-    #print(test)
-    # p = grid_search.predict(test)
-    #y_predicted = grid_search.predict(docs_test)
-    #print([p])
-
-    # Print the classification report
-    print(metrics.classification_report(y_test, y_predicted,
-                                        target_names=dataset.target_names))
+    y_test = svm.project(test_data)
+    i = 0
+    for a in y_test:
+        if a == "neg":
+            i = i + 1
+    print(i)
---
+++ spark-movie/python-processing/sentiment_analysis/linear_svm.py
@@ -0,0 +1,79 @@
+import os
+
+import pickle
+
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+class ProcessingContainer:
+    def __init__(self, vectorizer, svm):
+        self._vectorizer = vectorizer
+        self._svm = svm
+
+class LinearSVM:
+    def train(self, positive_folder_path, negative_folder_path):
+        vectorizer = TfidfVectorizer(min_df=3, max_df=0.95, sublinear_tf=True, use_idf=True)
+        svm = LinearSVC(C=1000)
+        self._processing_container = ProcessingContainer(vectorizer, svm)
+
+        train_data = []
+        train_labels = []
+        # Loads pos folder
+        for root, sub, files in os.walk(positive_folder_path):
+            for file_name in files:
+                url = os.path.join(root, file_name)
+                content = open(url, 'r', encoding='utf-8', errors='ignore').read()
+                train_data.append(content)
+                train_labels.append('pos')
+
+        len_positive = len(train_data)
+        if len_positive == 0:
+            print('[SVM]: The positive folder has not been found or is empty.')
+            exit(1)
+
+        # Loads pos folder
+        for root, sub, files in os.walk(negative_folder_path):
+            for file_name in files:
+                url = os.path.join(root, file_name)
+                content = open(url, 'r', encoding='utf-8', errors='ignore').read()
+                train_data.append(content)
+                train_labels.append('neg')
+
+        if len_positive == len(train_data):
+            print('[SVM]: The negative folder has not been found or is empty.')
+            exit(1)
+
+        # Computes frequencies
+        train_vectors = self._processing_container._vectorizer.fit_transform(train_data)
+        
+        # Trains the SVM
+        self._processing_container._svm.fit(train_vectors, train_labels)
+
+    def project(self, string):
+        test_vect = self._processing_container._vectorizer.transform(string)
+        return self._processing_container._svm.predict(test_vect)
+
+    def dump(self, path):
+        with open(path, 'wb') as output:
+            try:
+                pickle.dump(self._processing_container, output)
+            except:
+                print('[SVM]: The dump could not be save to \'{}\'.'.format(path))
+                exit(1)
+
+    def set_linear_svm(self, linear_svm):
+        self._svm = linear_svm
+
+    @staticmethod
+    def load(path):
+        try:
+            with open(path, 'rb') as input:
+                dump = pickle.load(input)
+                res = LinearSVM()
+                res._processing_container = dump
+                return res
+        except:
+            print('[SVM]: The dump \'{}\' could not be loaded.'.format(path))
+            exit(1)
+
+            return dump
